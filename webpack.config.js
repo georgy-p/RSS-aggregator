4621f081d99fb8c14172deec864350ebbbd5f27b
George <pigargeorge@gmail.com>
2022-03-05 13:37:12 +0300

    add new plugins

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,8 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WorkboxWebpackPlugin = require('workbox-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 const isProduction = process.env.NODE_ENV == 'production';
 
@@ -18,7 +20,13 @@ const config = {
     },
     plugins: [
         new HtmlWebpackPlugin({
-            template: 'index.html',
+            template: './src/index.html',
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].css', 
+        }),
+        new CleanWebpackPlugin({
+            cleanStaleWebpackAssets: false,
         }),
 
         // Add your plugins here
@@ -30,7 +38,10 @@ const config = {
                 test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
                 type: 'asset',
             },
-
+            {
+                test: /\.css$/i,
+                use: [MiniCssExtractPlugin.loader, "css-loader"],
+              },
             // Add your rules for custom modules here
             // Learn more about loaders from https://webpack.js.org/loaders/
         ],
