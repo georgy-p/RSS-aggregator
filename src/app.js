10d53db1f15f3db1b490b50ff504e138178c53f2
George <pigargeorge@gmail.com>
2022-03-09 19:41:57 +0300

    replace form control to formhandle.js

--- src/app.js
+++ src/app.js
@@ -1,36 +1,12 @@
-import { string } from "yup";
-import _ from "lodash";
-import { watchedState } from "./render";
-import { init } from "./init";
+import { i18nextInit } from "./init.js";
+import { formHandle } from "./formhandle.js";
 
 
 
 export default () => {
-  init().then(() => {
-    const formEl = document.querySelector('form');
-    formEl.addEventListener('submit', (e) => {
-    e.preventDefault();
-    const data = new FormData(e.target);
-    const link = data.get('url');
-    string().url().isValid(link)
-      .then((validStatus) =>  {
-        console.log(validStatus);
-        if (validStatus === true) {
-          if (!_.includes(watchedState.feeds, link)) {
-            watchedState.valid = true;
-            watchedState.feeds.push(link);
-            watchedState.feedbackStatus = 'done';
-          } else {
-            watchedState.valid = false;
-            watchedState.feedbackStatus = 'dublicate';
-          }
-        } else {
-          watchedState.valid = false;
-          watchedState.feedbackStatus = 'error';
-        }
-      });
-    })
-  })
+  i18nextInit()
+    .then(() => formHandle()
+      
+    );
 };
-
 
