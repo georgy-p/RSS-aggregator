04d845a9e9b7327f68b8412b4e67aaee797cee97
George <pigaritunes@gmail.com>
2022-03-09 13:56:46 +0300

    add init function

--- src/app.js
+++ src/app.js
@@ -1,27 +1,35 @@
 import { string } from "yup";
 import _ from "lodash";
 import { watchedState } from "./render";
+import { init } from "./init";
+
+
 
 export default () => {
-  const formEl = document.querySelector('form');
-  formEl.addEventListener('submit', (e) => {
+  init().then(() => {
+    const formEl = document.querySelector('form');
+    formEl.addEventListener('submit', (e) => {
     e.preventDefault();
     const data = new FormData(e.target);
     const link = data.get('url');
     string().url().isValid(link)
       .then((validStatus) =>  {
+        console.log(validStatus);
         if (validStatus === true) {
           if (!_.includes(watchedState.feeds, link)) {
             watchedState.valid = true;
             watchedState.feeds.push(link);
+            watchedState.feedbackStatus = 'done';
           } else {
             watchedState.valid = false;
+            watchedState.feedbackStatus = 'dublicate';
           }
         } else {
           watchedState.valid = false;
+          watchedState.feedbackStatus = 'error';
         }
       });
-      
+    })
   })
 };
 
