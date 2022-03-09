cec3a2c07a3ab03d16fc162152f2d367dcf607b2
George <pigaritunes@gmail.com>
2022-03-09 13:55:27 +0300

    create locales directory

---
+++ src/locales/ru.js
@@ -0,0 +1,15 @@
+export default {
+  translation: {
+    header: 'RSS агрегатор',
+    subheader: 'Начните читать RSS сегодня! Это легко, это красиво.',
+    formText: 'Ссылка RSS',
+    addButton: 'Добавить',
+    exampleText: 'Пример: https://ru.hexlet.io/lessons.rss',
+    feedback: {
+      error: 'Ссылка должна быть валидным URL',
+      success: 'RSS успешно загружен',
+      dublicate: 'RSS уже существует',
+      notValidRss: 'Ресурс не содержит валидный RSS',
+    }
+  }
+};
