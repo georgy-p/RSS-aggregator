3d2b6f22c5ff587c46803c86a3556b25f4357356
George <pigargeorge@gmail.com>
2022-03-23 17:14:59 +0300

    name fixing

--- src/app.js
+++ src/app.js
@@ -1,12 +1,10 @@
 import { i18nextInit } from "./init.js";
-import { formHandle } from "./formhandle.js";
+import formHandle from "./formhandle.js";
 
 
 
 export default () => {
   i18nextInit()
-    .then(() => formHandle()
-      
-    );
+  .then(() => formHandle())
 };
 
