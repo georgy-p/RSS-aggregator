000e6d8af317599dca359d1fad75a1abc47319d4
George <pigargeorge@gmail.com>
2022-03-23 17:19:40 +0300

    add feeds, posts and modal render logic

--- src/render.js
+++ src/render.js
@@ -1,16 +1,38 @@
 import onChange from "on-change";
 import { state, elements } from "./init.js";
+import ru from "./locales/ru.js";
+import * as r from "./rendertools.js";
 
 
 
-export const watchedState = onChange(state, (path) => {
-  if (path === 'valid') {
-    if (state.valid === false) {
-      elements.form.inputEl.classList.add('is-invalid');
-    } else {
-      elements.form.inputEl.classList.remove('is-invalid')
-    }
-      elements.form.formEl.reset();
-      elements.form.formEl.focus();
+export const watchedState = onChange(state, (path, value) => {
+  if (path === 'feedbackError') {
+    elements.form.inputEl.classList.add('is-invalid');
+    elements.feedbackEl.classList.add('text-danger');
+    elements.feedbackEl.textContent = value;
+  } else {
+    elements.form.inputEl.classList.remove('is-invalid');
+    elements.feedbackEl.classList.remove('text-danger');
+    elements.feedbackEl.classList.add('text-success');
+    elements.feedbackEl.textContent = ru.translation.feedback.success;
   }
+    elements.form.formEl.reset();
+    elements.form.formEl.focus();
+
+    if (path === 'content.feeds') {
+      r.renderFeeds(value);
+    }
+
+    if (path === 'content.posts') {
+      console.log('This is posts:');
+      r.renderPosts(value);
+    }
+
+    if (path === 'modalId') {
+      if (value !== null) {
+        r.renderModal(value);
+      } else {
+        r.closeModal(value);
+      }
+    }
 });
