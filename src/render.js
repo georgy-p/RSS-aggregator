4d35af855970ab31b073d25a5ebd2928dafcbcd3
George <pigargeorge@gmail.com>
2022-03-29 14:23:00 +0300

    fix feedbackStatus work, delete form.reset() for error situation

--- src/render.js
+++ src/render.js
@@ -1,30 +1,32 @@
 import onChange from "on-change";
 import { state, elements } from "./init.js";
 import ru from "./locales/ru.js";
-import * as r from "./rendertools.js";
+import * as r from "./renderhandle.js";
 
 
 
 export const watchedState = onChange(state, (path, value) => {
-  if (path === 'feedbackError') {
-    elements.form.inputEl.classList.add('is-invalid');
-    elements.feedbackEl.classList.add('text-danger');
-    elements.feedbackEl.textContent = value;
-  } else {
-    elements.form.inputEl.classList.remove('is-invalid');
-    elements.feedbackEl.classList.remove('text-danger');
-    elements.feedbackEl.classList.add('text-success');
-    elements.feedbackEl.textContent = ru.translation.feedback.success;
+  if (path === 'feedbackStatus') {
+    if (value === 'downloaded') {
+      elements.form.inputEl.classList.remove('is-invalid');
+      elements.feedbackEl.classList.remove('text-danger');
+      elements.feedbackEl.classList.add('text-success');
+      elements.feedbackEl.textContent = ru.translation.feedback.success;
+      elements.form.formEl.reset();
+      elements.form.formEl.focus();
+    } else {
+      elements.form.inputEl.classList.add('is-invalid'); 
+      elements.feedbackEl.classList.add('text-danger');
+      elements.feedbackEl.textContent = value;
+      elements.form.formEl.focus();
+    }
   }
-    elements.form.formEl.reset();
-    elements.form.formEl.focus();
 
     if (path === 'content.feeds') {
       r.renderFeeds(value);
     }
 
     if (path === 'content.posts') {
-      console.log('This is posts:');
       r.renderPosts(value);
     }
 
