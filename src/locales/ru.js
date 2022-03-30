bc9f0212338f0c6cb75ce0a9b8b9ebaa7060b877
George <pigargeorge@gmail.com>
2022-03-30 15:46:41 +0300

    add new text for buttons and modal

--- src/locales/ru.js
+++ src/locales/ru.js
@@ -3,15 +3,24 @@ export default {
     header: 'RSS агрегатор',
     subheader: 'Начните читать RSS сегодня! Это легко, это красиво.',
     formText: 'Ссылка RSS',
-    addButton: 'Добавить',
     exampleText: 'Пример: https://ru.hexlet.io/lessons.rss',
     feedback: {
       errors: {
         invalid: 'Ссылка должна быть валидным URL',
         dublicate: 'RSS уже существует',
         notValidRss: 'Ресурс не содержит валидный RSS',
+        networkError: 'Ошибка сети',
+        notEmpty: 'Не должно быть пустым',
       },
       success: 'RSS успешно загружен',
-    }
+    },
+    buttons: {
+      add: 'Добавить',
+      read: 'Просмотр',
+    },
+    modal: {
+      read: 'Читать полностью',
+      close: 'Закрыть',
+    },
   }
 };
