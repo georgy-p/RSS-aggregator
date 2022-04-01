a942d8f275188e82df73c40679489b570d86dcc2
George <pigargeorge@gmail.com>
2022-04-01 13:13:00 +0300

    delete unused rules

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,6 @@
-// Generated using webpack-cli https://github.com/webpack/webpack-cli
 import path from 'path';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import WorkboxWebpackPlugin from 'workbox-webpack-plugin';
-import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import { CleanWebpackPlugin } from 'clean-webpack-plugin';
 
 const isProduction = process.env.NODE_ENV == 'production';
@@ -11,7 +9,7 @@ const isProduction = process.env.NODE_ENV == 'production';
 const config = {
     entry: './src/index.js',
     output: {
-        path: path.resolve('./', 'public'),
+        path: path.resolve('./', 'dist'),
     },
     devServer: {
         open: true,
@@ -21,30 +19,22 @@ const config = {
         new HtmlWebpackPlugin({
             template: './index.html',
         }),
-        new MiniCssExtractPlugin({
-            filename: '[name].css', 
-        }),
         new CleanWebpackPlugin({
             cleanStaleWebpackAssets: false,
         }),
 
-        // Add your plugins here
-        // Learn more about plugins from https://webpack.js.org/configuration/plugins/
     ],
     module: {
         rules: [
-            {
-                test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
-                type: 'asset',
-            },
-            {
-                test: /\.css$/i,
-                use: [MiniCssExtractPlugin.loader, "css-loader"],
-              },
-            // Add your rules for custom modules here
-            // Learn more about loaders from https://webpack.js.org/loaders/
+          {
+            test: /\.css$/,
+            use: [
+              'style-loader',
+              'css-loader',
+            ],
+          },
         ],
-    },
+      },
 };
 
 export default () => {
