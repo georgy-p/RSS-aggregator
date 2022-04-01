c26375fa0e010f995e5bd064fac227fb32e5f659
George <pigargeorge@gmail.com>
2022-04-01 12:15:38 +0300

    join all functions together, add state and i18next instances

--- src/app.js
+++ src/app.js
@@ -1,65 +1,92 @@
-import { i18nextInit, elements } from "./init.js";
-import { string, } from "yup";
+import { elements } from "./handles/elements.js";
+import { string } from "yup";
 import _ from "lodash";
-import * as rss from "./rsshandle.js";
-import { watchedState } from "./render.js";
+import onChange from "on-change";
 import ru from "./locales/ru.js";
+import * as rss from "./handles/rsshandle.js";
+import * as r from "./handles/renderhandle.js";
 
-const schemaIsValid = string().url();
-const schemaHasDublicate = string().test(
-  'has-dublicate',
-  () => ru.translation.feedback.errors.dublicate,
-  (value) => !watchedState.content.links.includes(value),
-);
-const schemaHasValidRss = string().test(
-  'has-valid RSS',
-  () => ru.translation.feedback.errors.notValidRss,
-  (value) => rss.isValidRss(value),
-);
 
-const rssTimer = () => rss.getContent().then(() => setTimeout(rssTimer, 5000));
 
-const containerHandle = () => {
-    const postsContainer = elements.content.posts;
-    postsContainer.addEventListener('click', (e) => {
-      const currentPost = e.target;
-      if (currentPost.type === 'button') {
-        const postId = currentPost.dataset.id;
-        const postData = _.find(watchedState.content.posts, { id: postId });
-        const postTitle = postData.postTitle;
-        watchedState.content.readed.push(postTitle);
-        watchedState.modalId = postData;
+
+export default (state, i18nextInstance) => {
+  const watchedState = onChange(state, (path, value) => {
+    if (path === 'feedbackStatus') {
+      if (value === 'downloaded') {
+        r.renderFeedbackOk(i18nextInstance);
+      } else {
+        r.renderFeedbackProblem(value, i18nextInstance);
       }
-    });
+    }
   
-    const modalContainer = elements.modal.mainDiv;
-    modalContainer.addEventListener('click', (e) => {
-      if (e.target.type === 'button') {
-        watchedState.modalId = null;
+      if (path === 'content.feeds') {
+        r.renderFeeds(value, i18nextInstance);
       }
-    });
+  
+      if (path === 'content.posts') {
+        r.renderPosts(value, watchedState.content.readed, i18nextInstance);
+      }
+  
+      if (path === 'modalId') {
+        if (value !== null) {
+          r.renderModal(value);
+        } else {
+          r.closeModal(value);
+        }
+      }
+  });
 
+  const postsContainer = elements.content.posts;
+  postsContainer.addEventListener('click', (e) => {
+  const currentPost = e.target;
+    if (currentPost.type === 'button') {
+      const postId = currentPost.dataset.id;
+      const postData = _.find(watchedState.content.posts, { id: postId });
+      const postTitle = postData.postTitle;
+      watchedState.content.readed.push(postTitle);
+      watchedState.modalId = postData;
+    }
+  });
+  
+  const modalContainer = elements.modal.mainDiv;
+  modalContainer.addEventListener('click', (e) => {
+    if (e.target.type === 'button') {
+      watchedState.modalId = null;
+    }
+  });
+
+  const schemaIsValid = string().url();
+  const schemaHasDublicate = string().test(
+    'has-dublicate',
+    () => ru.translation.feedback.errors.dublicate,
+    (value) => !watchedState.content.links.includes(value),
+  );
+  const schemaHasValidRss = string().test(
+    'has-valid RSS',
+    () => ru.translation.feedback.errors.notValidRss,
+    (value) => rss.isValidRss(value),
+  );
+
+  const rssTimer = () => rss.getContent(watchedState).then(() => setTimeout(rssTimer, 5000));
+  
     const formContainer = elements.form.formEl;
     formContainer.addEventListener('submit', (e) => {
       e.preventDefault();
       const data = new FormData(e.target);
       const link = data.get('url');
-      return schemaIsValid.validate(link)
+      schemaIsValid.validate(link)
         .then(() => schemaHasDublicate.validate(link))
         .then(() => schemaHasValidRss.validate(link))
         .then(() => {
           watchedState.content.links.push(link)
-          rss.getContent()
+          rss.getContent(watchedState)
         })
         .then(() => watchedState.feedbackStatus = 'downloaded')
         .then(() => setTimeout(rssTimer, 5000))
-        .catch((e) => watchedState.feedbackStatus = e.errors.join())
-      });
-};
-
-export default () => {
-  i18nextInit()
-  .then(() => containerHandle())
-  .catch((e) => watchedState.feedbackStatus = e.errors.join())
+        .catch((e) => {
+          console.log('Alarm!');
+          watchedState.feedbackStatus = e.errors.join()
+        })
+      })
 };
 
