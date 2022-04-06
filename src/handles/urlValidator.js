e995b1eb80783fb563f41620c229f1a934a88914
George <pigaritunes@gmail.com>
2022-04-06 14:09:06 +0300

    separate rssValidator

--- src/handles/urlValidator.js
+++ src/handles/urlValidator.js
@@ -1,16 +1,19 @@
 import * as yup from "yup";
 import { isValidRss } from "./rsshandle.js";
 
-export default (url, feedsList) => {
+export const urlValidator = (url, feedsList) => {
   const schemaUrl = yup
     .string()
     .required('notEmpty')
     .url('invalid')
     .notOneOf(feedsList, 'dublicate')
-    .test(
-      'has-valid RSS',
-      () => 'invalidUrl',
-      (value) => isValidRss(value))
-
   return schemaUrl.validate(url);
 };
+
+export const rssValidator = (url) => {
+  const schemaRss = yup.string().url().test(
+    'has-valid RSS',
+    () => 'invalidUrl',
+    (value) => isValidRss(value));
+  return schemaRss.validate(url);
+}
